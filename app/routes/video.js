5c8cf632ca02d5396599444d699dddf124961a65
Jeff Keen <jeff@keen.me>
2017-10-25 23:09:25 -0500

    Add custom youtube id logic

--- app/routes/video.js
+++ app/routes/video.js
@@ -2,7 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var videoId = params.title.match(/\-([0-9])$/)[1];
-    return this.store.find('video', videoId);
+    let ytid = params.title.match(/\-([0-9A-Za-z]+)$/);
+    if (ytid && ytid[1] && ytid[1].length > 1) {
+      return this.store.createRecord('video', {
+          id: ytid[1],
+          name: "",
+          url: `https://www.youtube.com/embed/${ytid[1]}?autoplay=true&iv_load_policy=3`,
+          ytid: ytid[1],
+          imageName: ""
+        });
+    }
+    else {
+      var videoId = params.title.match(/\-([0-9])+$/)[1];
+      return this.store.find('video', videoId);
+    }
   }
 });
