f829d2e378c6478e2422f03314c553ef5e8aecef
Jeff Keen <jeff@keen.me>
2020-06-30 22:44:23 -0500

    Ember 3.1, you're too old

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,9 @@
 import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
+import { scheduleOnce } from '@ember/runloop';
+import { getWithDefault } from '@ember/object';
 
 export default Route.extend({
   metrics: service(),
@@ -9,14 +12,20 @@ export default Route.extend({
 
   init() {
     this._super(...arguments);
-
-    let router = this.router;
-    router.on('routeDidChange', () => {
-      const page = router.currentURL;
-      const title = router.currentRouteName || 'unknown';
-
-      this.metrics.trackPage({ page, title });
-    });
+    this._trackPage();
   },
 
+  didTransition() {
+    this._super(...arguments);
+    this._trackPage();
+  },
+
+  _trackPage() {
+    scheduleOnce('afterRender', this, () => {
+      const page = document.location.href;
+      const title = getWithDefault(this, 'routeName', 'unknown');
+
+      get(this, 'metrics').trackPage({ page, title });
+    });
+  }
 });
